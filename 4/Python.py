22ab60c4448f0fe96cad18ebb3a88976bcfb7730
Alekhya Ghosh <officialalekhya2004@gmail.com>
2026-07-01 00:09:27 +0530

    Add initial implementations for string manipulation functions in Python.py, including reverse string, palindrome check, vowel/consonant count, and word count.

---
+++ 4/Python.py
@@ -0,0 +1,34 @@
+# Reverse a string
+'''name="Alekhya"
+print(name[::-1])'''
+
+# Check palindrome string
+'''name="madam"
+if name==name[::-1]:
+    print("Palindrome")
+else:
+    print("Not Palindrome")'''
+
+# Count vowels and consonants
+'''name="Alekhya"
+
+vowels=0
+consonants=0
+for ch in name.lower():
+    if ch.isalpha():
+        if ch in 'aeiou':
+            vowels+=1
+        else:
+            consonants+=1
+print("Vowels",vowels)
+print("Consonants",consonants)'''
+
+# Count words in a sentence
+
+'''sentence="My name is Alekhya"
+
+word=sentence.split()
+
+print("Total Words",len(word))'''
+# USING SPLIT() FUNCTION
+
